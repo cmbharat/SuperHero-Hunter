886f0366979a0a5cc3d7b981aaddf55ecf8b584e
cmbharat <bharatcm70@gmail.com>
2023-03-06 18:06:26 +0530

    cleanup of code and file structure

--- js/favourites.js
+++ js/favourites.js
@@ -19,16 +19,12 @@ function favouritesDetails(favourites) {
     let parentDiv = document.getElementsByClassName("superhero");
 
     let pdiv = parentDiv[0];
-
-    console.log("inside getFavouriteDetails method", heroID);
     var url = `https://gateway.marvel.com:443/v1/public/characters/${heroID}?ts=1&apikey=7c6d0ba7a6a8eba22f225d33d5adba3f&hash=1cb87892a3e91f8474f97b26817854c6`;
 
     fetch(url)
       .then((response) => response.json())
       .then((data) => {
-        console.log("inside fetch data", data);
         var heroData = data.data.results;
-        console.log("herodata", heroData);
         let childDiv = document.createElement("div");
 
         childDiv.innerHTML = `
